05323613fd01e09928e1bdc2e53852e6cb33e344
rubyist <scott.barron@github.com>
2015-03-05 09:56:29 -0500

    Read header, bytes left, and version

--- decoder.go
+++ decoder.go
@@ -1,15 +1,62 @@
 package drum
 
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"os"
+)
+
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
 // rest of the data.
 // TODO: implement
 func DecodeFile(path string) (*Pattern, error) {
-	p := &Pattern{}
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	header := make([]byte, 6)
+	file.Read(header)
+	if string(header) != "SPLICE" {
+		return nil, errors.New("Invalid splice file")
+	}
+
+	byReader := bufio.NewReader(file)
+	var byLeft int64
+	err = binary.Read(byReader, binary.BigEndian, &byLeft)
+	if err != nil {
+		return nil, err
+	}
+
+	version, err := byReader.ReadBytes('\x00')
+	if err != nil {
+		return nil, err
+	}
+
+	p := &Pattern{
+		Version: string(version),
+	}
 	return p, nil
 }
 
 // Pattern is the high level representation of the
 // drum pattern contained in a .splice file.
 // TODO: implement
-type Pattern struct{}
+type Pattern struct {
+	Version string
+	Tempo   float64
+	Tracks  []*Track
+}
+
+func (p *Pattern) String() string {
+	return ""
+}
+
+type Track struct {
+	Id    int
+	Name  string
+	Steps [16]bool
+}
