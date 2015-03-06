f2d0f3222e56f09016e6d2c0c0af2cf8d78e6c83
rubyist <scott.barron@github.com>
2015-03-05 19:51:59 -0500

    Rename a few things

--- decoder.go
+++ decoder.go
@@ -13,79 +13,78 @@ import (
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
 // rest of the data.
-// TODO: implement
 func DecodeFile(path string) (*Pattern, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
+	buf := bufio.NewReader(file)
 
 	// Read the SPLICE header
 	header := make([]byte, 6)
-	file.Read(header)
+	buf.Read(header)
 	if string(header) != "SPLICE" {
 		return nil, errors.New("Invalid splice file")
 	}
 
 	// Next up is an integer containing the number of bytes left to read
-	byReader := bufio.NewReader(file)
-	var byLeft int64
-	err = binary.Read(byReader, binary.BigEndian, &byLeft)
+	var remaining int64
+	err = binary.Read(buf, binary.BigEndian, &remaining)
 	if err != nil {
 		return nil, err
 	}
 
 	// The 32 byte long version string
 	version := make([]byte, 32)
-	_, err = byReader.Read(version)
+	_, err = buf.Read(version)
 	if err != nil {
 		return nil, err
 	}
 	version = bytes.Trim(version, "\x00")
-	byLeft -= 32
+	remaining -= 32
 
 	// Tempo is a little endian 32 bit floating point
 	var tempo float32
-	err = binary.Read(byReader, binary.LittleEndian, &tempo)
+	err = binary.Read(buf, binary.LittleEndian, &tempo)
 	if err != nil {
 		return nil, err
 	}
-	byLeft -= 4
+	remaining -= 4
 
 	var tracks []*Track
-	for byLeft > 0 {
+	for remaining > 0 {
 		// Track id is a little endian 32 bit int
 		var id int32
-		err = binary.Read(byReader, binary.LittleEndian, &id)
+		err = binary.Read(buf, binary.LittleEndian, &id)
 		if err != nil {
 			return nil, err
 		}
-		byLeft -= 4
+		remaining -= 4
 
 		// A byte describing the length of the instrument name
-		l, err := byReader.ReadByte()
+		l, err := buf.ReadByte()
 		if err != nil {
 			return nil, err
 		}
-		byLeft--
+		remaining--
 
 		// The instrument name
 		name := make([]byte, l)
-		_, err = byReader.Read(name)
+		_, err = buf.Read(name)
 		if err != nil {
 			return nil, err
 		}
-		byLeft -= int64(l)
+		remaining -= int64(l)
 
 		// 16 steps, 1 byte each
 		steps := make([]bool, 16)
 		for i := 0; i < 16; i++ {
-			s, err := byReader.ReadByte()
+			s, err := buf.ReadByte()
 			if err != nil {
 				return nil, err
 			}
 			steps[i] = s == 1
-			byLeft--
+			remaining--
 		}
 
 		track := &Track{
@@ -106,7 +105,6 @@ func DecodeFile(path string) (*Pattern, error) {
 
 // Pattern is the high level representation of the
 // drum pattern contained in a .splice file.
-// TODO: implement
 type Pattern struct {
 	Version string
 	Tempo   float32
