9737b840048623ed2fa2c5213e54b9ee63e079bc
rubyist <scott.barron@github.com>
2015-03-12 13:14:05 -0400

    Start buffered mixer

--- example/play.go
+++ example/play.go
@@ -82,27 +82,25 @@ func main() {
 
 	//drum.Play(pat)
 	period := time.Millisecond * time.Duration(((1.0/(p.Tempo/60.0))/4.0)*1000.0)
+	log.Printf("Period: %v", period)
 
-	numTracks := len(p.Tracks)
-	buffers := make([][]int32, numTracks)
-	cursors := make([]int, numTracks)
-	for i := range cursors {
-		cursors[i] = -1
-	}
+	mixer := NewBufferedMixer(0)
 
-	for i, track := range p.Tracks {
+	for _, track := range p.Tracks {
 		fileName := track.Name + ".wav"
 		var info sndfile.Info
 		f, err := sndfile.Open(fileName, sndfile.Read, &info)
 		if err != nil {
 			log.Fatalf("error: %s", err)
 		}
+		log.Printf("Track %s, Frames: %d, Sample Rate: %d, Channels: %d", track.Name, info.Frames, info.Samplerate, info.Channels)
 		buffer := make([]int32, int(info.Frames)*int(info.Channels))
-		_, err = f.ReadItems(buffer)
+		_, err = f.ReadFrames(buffer)
 		if err != nil {
 			log.Fatal(err)
 		}
-		buffers[i] = buffer
+
+		mixer.Add(buffer, track.Steps)
 		f.Close()
 	}
 
@@ -110,63 +108,26 @@ func main() {
 
 	go func() {
 		<-start
-		step := 0
 		timer := time.NewTicker(period)
 		for {
 			<-timer.C
-			for i := 0; i < numTracks; i++ {
-				if p.Tracks[i].Steps[step] {
-					cursors[i] = 0
-				}
-			}
-
-			step++
-			step = step % 16
+			mixer.Tick()
 		}
 	}()
 
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-	//stream, err := portaudio.OpenDefaultStream(0, int(i.Channels), float64(i.Samplerate), 0, kick)
-	var v int32
 	var starter sync.Once
 	stream, err := portaudio.OpenDefaultStream(0, 2, 44100, 0, func(o []int32) {
 		starter.Do(func() { close(start) })
-
-		for i := range o {
-			v = 0
-			for i := 0; i < numTracks; i++ {
-				c := cursors[i]
-				if c >= 0 && c < len(buffers[i]) {
-					v += (buffers[i][c] / int32(numTracks+1))
-					cursors[i] = c + 1
-				}
-			}
-
-			o[i] = v
-		}
+		mixer.Read(o)
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stream.Close()
-
-	//if err = stream.Start(); err != nil {
-	//	log.Fatal(err)
-	//}
-
 	stream.Start()
-	/*
-		if err := stream.Write(); err != nil {
-			log.Printf("Got an error: %s", err)
-		}
-	*/
 	defer stream.Stop()
-	//if err := stream.Write(); err != nil {
-	//	log.Fatalf("Play error: %s", err)
-	//}
-
-	//stream.Stop()
 
 	start <- 1
 	for {
