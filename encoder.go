ae38974d9fd10a817738ef5b3035779142daccdd
rubyist <scott.barron@github.com>
2015-07-19 20:44:39 -0400

    Add all the other stuff that never got added

--- encoder.go
+++ encoder.go
@@ -1,11 +1,75 @@
 package drum
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/binary"
+	"io"
+	"log"
 	"os"
+	"unsafe"
 )
+
+type spliceWriter struct {
+	w     io.Writer
+	err   error
+	count uint64
+	b     bytes.Buffer
+}
+
+func (p *spliceWriter) write(buf []byte) {
+	if p.err != nil {
+		return
+	}
+
+	n, err := p.b.Write(buf)
+	if err != nil {
+		p.err = err
+		return
+	}
+
+	log.Printf("Increasing count by %d", n)
+	p.count += uint64(n)
+}
+
+func (p *spliceWriter) bwrite(order binary.ByteOrder, data interface{}) {
+	if p.err != nil {
+		return
+	}
+
+	// Should really use reflect and get the size?
+	err := binary.Write(&p.b, order, data)
+	if err != nil {
+		p.err = err
+		return
+	}
+
+	c := unsafe.Sizeof(data)
+
+	log.Printf("(b) Increasing count by %d", c)
+	p.count += uint64(c)
+}
+
+func (p *spliceWriter) flush() error {
+	if p.err != nil {
+		return p.err
+	}
+
+	// should handle short writes
+	if _, err := p.w.Write([]byte(spliceHeader)); err != nil {
+		return err
+	}
+
+	log.Printf("Writing a count of %d\n", p.count)
+	if err := binary.Write(p.w, binary.BigEndian, p.count); err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(p.w, &p.b); err != nil {
+		return err
+	}
+
+	return p.err
+}
 
 // Encode encodes the pattern to the file found at the provided path.
 func Encode(pat *Pattern, path string) error {
@@ -15,52 +79,28 @@ func Encode(pat *Pattern, path string) error {
 	}
 	defer file.Close()
 
-	buf := bufio.NewWriter(file)
-	// Write header
-	_, err = buf.WriteString(spliceHeader)
-	if err != nil {
-		return err
-	}
-
-	// Write bytes left
-	var total uint64
-	total = 32                           // version string
-	total += 4                           // tempo
-	total += uint64(len(pat.Tracks) * 4) // track ids
-	total += uint64(len(pat.Tracks) * 1) // inst lengths
-	for _, track := range pat.Tracks {
-		total += uint64(len(track.Name)) // inst names
-	}
-	total += uint64(len(pat.Tracks)) * 16 // steps
-	err = binary.Write(buf, binary.BigEndian, total)
-	if err != nil {
-		return err
-	}
+	buf := &spliceWriter{w: file}
 
 	// Write version string with null padding
-	buf.WriteString(version)
-	buf.Write(bytes.Repeat([]byte{0}, 32-len(version)))
+	buf.write([]byte(version))
+	buf.write(bytes.Repeat([]byte{0}, 32-len(version)))
 
 	// Write tempo
-	err = binary.Write(buf, binary.LittleEndian, pat.Tempo)
-	if err != nil {
-		return err
-	}
+	buf.bwrite(binary.LittleEndian, pat.Tempo)
 
 	// Write tracks
 	for _, track := range pat.Tracks {
-		binary.Write(buf, binary.LittleEndian, track.ID)
-		binary.Write(buf, binary.BigEndian, uint8(len(track.Name)))
-		buf.WriteString(track.Name)
+		buf.bwrite(binary.LittleEndian, track.ID)
+		buf.bwrite(binary.BigEndian, uint8(len(track.Name)))
+		buf.write([]byte(track.Name))
 		for _, step := range track.Steps {
 			if step {
-				buf.WriteByte(1)
+				buf.bwrite(binary.BigEndian, []byte{1})
 			} else {
-				buf.WriteByte(0)
+				buf.bwrite(binary.BigEndian, []byte{0})
 			}
 		}
 	}
 
-	buf.Flush()
-	return nil
+	return buf.flush()
 }
